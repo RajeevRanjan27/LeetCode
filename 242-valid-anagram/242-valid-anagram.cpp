2178675c3fbb9552d8a55f96cef39f3f62cf95f2
Rajeev Ranjan <rajeevr.ug20.cse@nitp.ac.in>
2022-07-29 02:45:33 +0530

    Time: 6 ms (90.64%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,24 +2,30 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
        
+        
         if(s.length()!=t.length())
-            return false;
-        
-        
-        int counts[26]={};
-        for(int i=0;i<s.length();i++)
         {
-            counts[s[i]-'a']++;
-        }
-        for(int i=0;i<t.length();i++)
-        {
-            counts[t[i]-'a']--;
-        }
-        for(auto x:counts)
-        { if(x)
             return false;
         }
-        return true;
+        else {
+        vector<int>freq(26,0);
+            for(int i=0;s[i]!=NULL;i++)
+            {
+                freq[s[i]-'a']++;
+            } 
+            for(int i=0;t[i]!=NULL;i++)
+            {
+                freq[t[i]-'a']--;
+            }
+            
+            for(auto x:freq)
+            {
+                if(x)
+                    return false;
+            }
+            return true;
+        }
+       
         
     }
 };
