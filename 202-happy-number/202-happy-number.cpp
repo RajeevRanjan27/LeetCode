1f358405cc27a4040fc3425eff74ff94bedbbd92
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-08-07 12:56:39 +0530

    Time: 8 ms (15.18%), Space: 5.8 MB (84.88%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+        int sum=0;
+        
+        while(sum!=1 || sum!=89){
+       while(n)
+       {
+           cout<<sum<<endl;
+            sum+=(n%10)*(n%10);
+            n/=10;
+       }
+        if(sum==1)
+            return true;
+            
+        if(sum==89)
+            return false;
+        n=sum;
+        sum=0;
+        }
+            
+       
+        
+        
+    }
+};
