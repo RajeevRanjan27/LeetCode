a0320e59186fc580f7acc28c6e1225b4ecab74e3
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-08-07 11:40:38 +0530

    Time: 10 ms (48.53%), Space: 10.2 MB (84.18%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -4,14 +4,28 @@ public:
         
         int n=nums.size();
         
-        int ans=1;
+        // int ans=1;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]==0)
+        //         return 0;
+        //     else
+        //         ans*=(nums[i]>0)?1:-1;
+        // }
+        // return ans;
+        
+        
+        int count=0;
         for(int i=0;i<n;i++)
         {
             if(nums[i]==0)
                 return 0;
-            else
-                ans*=(nums[i]>0)?1:-1;
+            if(nums[i]<0)
+            {
+                count++;
+            }
         }
-        return ans;
+        return (count%2)?-1:1;
+        
     }
 };
