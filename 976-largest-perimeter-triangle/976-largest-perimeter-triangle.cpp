985f2bc34933210ae9b00f66c94fccdaf0540320
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-08-07 10:23:49 +0530

    Time: 45 ms (80.61%), Space: 21.9 MB (21.52%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
@@ -7,10 +7,16 @@ public:
         
         for(int i=n-1;i>1;i--)
         {
-            if(nums[i]<nums[i-1]+nums[i-2])
+            if(nums[i]<(nums[i-1]+nums[i-2]))
           //      int temp=nums[i]+nums[i-1]+nums[i-2];
-                ans=max(ans,nums[i]+nums[i-1]+nums[i-2]);
-        }
+                
+                
+               // ans=max(ans,nums[i]+nums[i-1]+nums[i-2]);
+       
+               // ans=nums[i]+nums[i-1]+nums[i-2];
+               ans=max(ans,nums[i]+nums[i-1]+nums[i-2]);
+                 //   break;
+            }
         return ans;
         
     }
