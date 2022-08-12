099923a2c5e62a775e3793caf53a1a1285ceb79f
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-08-12 18:03:48 +0530

    Time: 6 ms (98.36%), Space: 10.8 MB (60.44%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<vector<int>>ans(r,vector<int>(c));
-        vector<int>temp;
-        int count=0;
-      if(r*c!=m*n)
-          return mat;
+//         int n=mat.size();
+//         int m=mat[0].size();
+//         vector<vector<int>>ans(r,vector<int>(c));
+//         vector<int>temp;
+//         int count=0;
+//       if(r*c!=m*n)
+//           return mat;
     
-      for(int i=0;i<n;i++)
-      {
-          for(int j=0;j<m;j++)
-          {
+//       for(int i=0;i<n;i++)
+//       {
+//           for(int j=0;j<m;j++)
+//           {
             
-              temp.push_back(mat[i][j]);
-          }
-      }
+//               temp.push_back(mat[i][j]);
+//           }
+//       }
         
-        for(int i=0;i<r;i++)
+//         for(int i=0;i<r;i++)
+//         {
+//             for(int j=0;j<c;j++)
+//             {
+//                 ans[i][j]=temp[count++];
+//             }
+//         }
+//         return ans;
+        
+        
+        int m=mat.size();
+        int n=mat[0].size();
+        
+        if(m*n!=r*c)
         {
-            for(int j=0;j<c;j++)
+            return mat;
+        }
+        vector<vector<int>>ans(r,vector<int>(c));
+        int rptr=0,cptr=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
             {
-                ans[i][j]=temp[count++];
+                ans[rptr][cptr++]=mat[i][j];
+                if(cptr==c)
+                {
+                    rptr++;cptr=0;
+                }
             }
         }
         return ans;
-        
         
         
     }
