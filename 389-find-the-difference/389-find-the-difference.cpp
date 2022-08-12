17ee932f0728e3b019295d30f606d47dbf77afb9
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-08-12 19:13:27 +0530

    Time: 3 ms (78.43%), Space: 6.5 MB (88.69%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int>freq(26,0);
-        char ans;
-        for(int i=0;i<s.length();i++)
-        {
-            freq[s[i]-'a']++;
-        }
+//         vector<int>freq(26,0);
+//         char ans;
+//         for(int i=0;i<s.length();i++)
+//         {
+//             freq[s[i]-'a']++;
+//         }
+//         for(int i=0;i<t.length();i++)
+//         {
+//          freq[t[i]-'a']--;
+//          if(freq[t[i]-'a']<0)
+//          {
+//            // ans=(char)t[i];
+//              ans=t[i];
+//              break;
+//              // return ans;
+//          }
+         
+//         }
+//          return ans;
+        
+        int x=0;
         for(int i=0;i<t.length();i++)
         {
-         freq[t[i]-'a']--;
-         if(freq[t[i]-'a']<0)
-         {
-           ans=(char)t[i];
-            break;
-         }
-         
+            x+=t[i];
         }
-        return ans;
+        for(int j=0;j<s.length();j++)
+        {
+            x-=s[j];
+        }
+        return (char)x;
+        
         
         
       
