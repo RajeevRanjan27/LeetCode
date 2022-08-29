bd4cc7f377bd60eafca77bdf8c3b8f1c1357ce52
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-08-29 17:47:56 +0530

    Time: 3 ms (60.61%), Space: 8.3 MB (96.05%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,17 +11,74 @@
  */
 class Solution {
 public:
-    void postorder(TreeNode * root, vector<int>&v)
-    {
-        if(root==NULL)return ;
-        postorder(root->left,v);
-        postorder(root->right,v);
-        v.push_back(root->val);
-    }
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>v;
-        postorder(root,v);
-        return v;
+    // void postorder(TreeNode * root, vector<int>&v)
+    // {
+    //     if(root==NULL)return ;
+    //     postorder(root->left,v);
+    //     postorder(root->right,v);
+    //     v.push_back(root->val);
+    // }
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     vector<int>v;
+    //     postorder(root,v);
+    //     return v;
+//  vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int>post;
+//         stack<TreeNode*>st;
+//         TreeNode * curr=root;
+//         while(curr!=NULL||!st.empty())
+//         {
+//             if(curr!=NULL)
+//             {
+//                 st.push(curr);
+//                 curr=curr->left;
+                
+//             }
+//             else
+//             {
+//               TreeNode* temp=st.top()->right;
+//                 if(temp==NULL)
+//                 {
+//                     temp=st.top();
+//                     st.pop();
+//                     post.push_back(temp->val);
+//                     while(!st.empty() && temp==st.top()->right)
+//                     {
+//                         temp=st.top();st.pop();
+//                         post.push_back(temp->val);
+//                     }
+//                 }
+//                 else 
+//                 {
+//                     curr=temp;
+//                 }
+    
+//             }
+//         }
+//         return post;
+    
+    
+     vector<int> postorderTraversal(TreeNode* root) {
+         vector<int>post;
+         if(root==NULL)return post;
+         stack<TreeNode *>s;
+         s.push(root);
+         while(!s.empty())
+         {
+             TreeNode* node=s.top();
+             s.pop();
+             post.push_back(node->val);
+             if(node->left)s.push(node->left);
+             if(node->right)s.push(node->right);
+             
+         }
+         reverse(post.begin(),post.end());
+         return post;
+        
+        
+        
+        
+        
         
     }
 };
