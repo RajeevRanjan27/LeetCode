61201a6fa31e281522428974f771b0187b9a5940
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-01 01:02:38 +0530

    Time: 52 ms (61.35%), Space: 71.4 MB (51.57%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -12,97 +12,97 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-//         ListNode ans(0),* temp= &ans;;
+        ListNode ans(0),* temp= &ans;;
        
-//         ListNode * a;
-//         ListNode * b;
+        ListNode * a;
+        ListNode * b;
        
-//         a=l1;
-//         b=l2;
-//         int n=0,sum=0;
-//         while(a && b)
-//         {
+        a=l1;
+        b=l2;
+        int n=0,sum=0;
+        while(a && b)
+        {
             
            
-//             sum=(a->val+b->val +n);
-//             n=sum/10; 
-//             temp->next=new ListNode(sum%10);
-//             temp=temp->next;
-//             a=a->next;
-//             b=b->next;
-//         }
-        
-//         while(a!=NULL)
-//         {
-            
-            
-//              sum=(a->val+n);
-//             n=sum/10; 
-//             temp->next=new ListNode(sum%10);
-//              temp=temp->next;
-//             a=a->next;
-            
-//         }
-//         while(b!=NULL)
-//         {
-//             sum=(b->val+n);
-//             n=sum/10; 
-//             temp->next=new ListNode(sum%10);
-//             temp=temp->next;
-//             b=b->next;
-//         }
-//         if(n)
-//         {
-//             temp->next =new ListNode(n);
-//             temp=temp->next;
-//         }
-//         temp=NULL;
-        
-        
-        
-        
-//         return ans.next;
-        
-        ListNode ans(0), *temp=&ans;
-        ListNode *a=l1,*b=l2;
-        int carry=0,sum=0;
-        
-        while(a&&b)
-        {
-            sum=a->val+b->val+carry;
-            carry=sum/10;
+            sum=(a->val+b->val +n);
+            n=sum/10; 
             temp->next=new ListNode(sum%10);
             temp=temp->next;
             a=a->next;
             b=b->next;
-            
         }
+        
         while(a!=NULL)
         {
-            sum=a->val+carry;
-            carry=sum/10;
+            
+            
+             sum=(a->val+n);
+            n=sum/10; 
             temp->next=new ListNode(sum%10);
-            temp=temp->next;
+             temp=temp->next;
             a=a->next;
             
         }
         while(b!=NULL)
         {
-            sum=b->val+carry;
-            carry=sum/10;
-            temp->next=new ListNode (sum%10);
+            sum=(b->val+n);
+            n=sum/10; 
+            temp->next=new ListNode(sum%10);
             temp=temp->next;
             b=b->next;
         }
-        if(carry)
+        if(n)
         {
-            temp->next=new ListNode(carry);
+            temp->next =new ListNode(n);
             temp=temp->next;
-            
         }
         temp=NULL;
         
+        
+        
+        
         return ans.next;
+        
+//         ListNode ans(0), *temp=&ans;
+//         ListNode *a=l1,*b=l2;
+//         int carry=0,sum=0;
+        
+//         while(a&&b)
+//         {
+//             sum=a->val+b->val+carry;
+//             carry=sum/10;
+//             temp->next=new ListNode(sum%10);
+//             temp=temp->next;
+//             a=a->next;
+//             b=b->next;
+            
+//         }
+//         while(a!=NULL)
+//         {
+//             sum=a->val+carry;
+//             carry=sum/10;
+//             temp->next=new ListNode(sum%10);
+//             temp=temp->next;
+//             a=a->next;
+            
+//         }
+//         while(b!=NULL)
+//         {
+//             sum=b->val+carry;
+//             carry=sum/10;
+//             temp->next=new ListNode (sum%10);
+//             temp=temp->next;
+//             b=b->next;
+//         }
+//         if(carry)
+//         {
+//             temp->next=new ListNode(carry);
+//             temp=temp->next;
+            
+//         }
+//         temp=NULL;
+        
+//         return ans.next;
             
         
         
