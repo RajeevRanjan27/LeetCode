0a7cddc940f84238e897bee4ee5e9374025ad9b2
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-12 18:39:03 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (21.91%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -15,38 +15,53 @@ public:
         
          
             
-        int diff=0,flag=1,mismatch=0;
-        vector<int>vec(26);
-        int n=s1.length();
+//         int diff=0,flag=1,mismatch=0;
+//         vector<int>vec(26);
+//         int n=s1.length();
         
-        for(int i=0;i<n;i++)
-        {
-            vec[s1[i]-'a']++;
-            vec[s2[i]-'a']--;
-        }
-            for(auto freqarr:vec)
-            {
-                if(freqarr)
-                {
-                    flag=0;
-                    break;
-                }
-            }
-        if(!flag)
-        {
-            return false;
+//         for(int i=0;i<n;i++)
+//         {
+//             vec[s1[i]-'a']++;
+//             vec[s2[i]-'a']--;
+//         }
+//         for(auto freqarr:vec)
+//             {
+//                 if(freqarr)
+//                 {
+//                     flag=0;
+//                     break;
+//                 }
+//             }
+//         if(!flag)
+//         {
+//             return false;
             
-        }
+//         }
         
         
+//         for(int i=0;i<n;i++)
+//         {
+//             if(s1[i]!=s2[i])
+//             {
+//                 mismatch++;
+//             }
+//         }
+//         return mismatch<=2;
+        
+        
+        int cnt=0,idx1=0,idx2=0;
+        int n=s1.size();
+    
         for(int i=0;i<n;i++)
         {
             if(s1[i]!=s2[i])
             {
-                mismatch++;
+                (cnt++ ? idx2:idx1)=i;
+                
             }
         }
-        return mismatch<=2;
+        return cnt==0 || (cnt==2 && s1[idx1]==s2[idx2]&& s2[idx1]==s1[idx2]);
+        
         
         
     }
