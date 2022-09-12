9a2392967e1dda162acf52fc323146a2e90e6d2a
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-12 21:13:15 +0530

    Time: 551 ms (8.40%), Space: 144.8 MB (25.41%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -12,43 +12,55 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-//         if(root==NULL)return 0;
+        if(root==NULL)return 0;
 //         if(root->left==NULL && root->right==NULL)return 1;
         
 //         if(root->left==NULL)return minDepth(root->right)+1;
 //         if(root->right==NULL) return minDepth(root->left)+1;
-   
+
+        int lh=minDepth(root->left);
+        int rh=minDepth(root->right);
+        
+        if(root->left==NULL)return 1+rh;
+        if(root->right==NULL)return 1+lh;
         
         
-// //         int lh=minDepth(root->left);
-// //         int rh=minDepth(root->right);
-//         // if(lh==0 ||rh==0)return 1;
-//         return 1+min(minDepth(root->left),minDepth(root->right));
+        
+
+        return 1+min(lh,rh);
         
         
-        if(root==NULL)return 0;
+//         if(root==NULL)return 0;
         
-        queue<TreeNode*>q;
-        q.push(root);
-        int ans=1;
-        while(!q.empty())
-        {
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         int ans=1;
+//         while(!q.empty())
+//         {
         
-            int sz=q.size();
-            for(int i=0;i<sz;i++)
-            {
-                TreeNode * node=q.front();
-                q.pop();
-                if(node->left==NULL && node->right==NULL)return ans;
-                if(node->left!=NULL)q.push(node->left);
-                if(node->right!=NULL)q.push(node->right);
+//             int sz=q.size();
+//             for(int i=0;i<sz;i++)
+//             {
+//                 TreeNode * node=q.front();
+//                 q.pop();
+//                 if(node->left==NULL && node->right==NULL)return ans;
+//                 if(node->left!=NULL)q.push(node->left);
+//                 if(node->right!=NULL)q.push(node->right);
                 
                 
-            }
-            ans++;
+//             }
+//             ans++;
             
-        }
-        return ans;
+//         }
+//         return ans;
+        
+           
+//         if(root==NULL)return 0;
+//         int lh=minDepth(root->left);
+//         int rh=minDepth(root->right);
+//         if(lh==0||rh==0)return 1;
+        
+//         return 1+ min(lh,rh);
         
     }
 };
