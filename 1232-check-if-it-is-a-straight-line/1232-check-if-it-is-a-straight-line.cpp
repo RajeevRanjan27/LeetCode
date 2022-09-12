0aff03c6b4a2606fd7731475c3d14ec33eb13673
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-12 19:40:38 +0530

    Time: 26 ms (7.28%), Space: 10.1 MB (77.16%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -2,24 +2,35 @@ class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
         
-      float y = coordinates[1][1]-coordinates[0][1];
-        float x=coordinates[1][0]-coordinates[0][0];
+//       float y = coordinates[1][1]-coordinates[0][1];
+//         float x=coordinates[1][0]-coordinates[0][0];
         
-       // float m=(x==0)?0:y/x;
+//        // float m=(x==0)?0:y/x;
+        
+//         for(int i=2;i<coordinates.size();i++)
+//         {
+            
+// //             float tempx=coordinates[i][0]-coordinates[i-1][0];
+// //              float tempy = coordinates[i][1]-coordinates[i-1][1];
+// //             float tempm=(tempx)?(tempy/tempx):0;
+        
+// //             if(m!=tempm)
+//             if(x*(coordinates[i][1]-coordinates[0][1])!=(coordinates[i][0]-coordinates[0][0])*y)
+            
+//                 return false;
+//         }
+//         return true;
+        
+        
+        
+        float y=coordinates[1][1]-coordinates[0][1];
+        float x=coordinates[1][0]-coordinates[0][0];
         
         for(int i=2;i<coordinates.size();i++)
         {
-            
-//             float tempx=coordinates[i][0]-coordinates[i-1][0];
-//              float tempy = coordinates[i][1]-coordinates[i-1][1];
-//             float tempm=(tempx)?(tempy/tempx):0;
-        
-//             if(m!=tempm)
-            if(x*(coordinates[i][1]-coordinates[0][1])!=(coordinates[i][0]-coordinates[0][0])*y)
-            
+            if(x*(coordinates[i][1]-coordinates[1][1])!=y*(coordinates[i][0]-coordinates[1][0]))
                 return false;
         }
         return true;
-        
     }
 };
