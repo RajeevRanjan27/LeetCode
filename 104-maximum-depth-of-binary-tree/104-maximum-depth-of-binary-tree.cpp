ddd1728d079d601de441a4890b2c8f58c93b83ac
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-12 20:45:05 +0530

    Time: 11 ms (75.57%), Space: 18.8 MB (55.22%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -13,33 +13,32 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         
-        
-        
-        
-//         if(root==NULL)return 0;
-//         int lh=maxDepth(root->left);
-//         int rh=maxDepth(root->right);
-//         return 1+max(lh,rh);
-        
-    
-    int ans=0;
-        if(root==NULL)return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty())
-        {
-            int sz=q.size();
-            for(int i=0;i<sz;i++)
-            {
-                TreeNode* node=q.front();
-                q.pop();
-                if(node->left!=NULL)q.push(node->left);
-                if(node->right!=NULL)q.push(node->right);
+        if(root==NULL)return 0;
+        int lh=maxDepth(root->left);
+        int rh=maxDepth(root->right);
+        return 1+ max(lh,rh);
                 
-            }
-            ans++;
-        }
-        return ans;
+        
+
+    
+//     int ans=0;
+//         if(root==NULL)return ans;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty())
+//         {
+//             int sz=q.size();
+//             for(int i=0;i<sz;i++)
+//             {
+//                 TreeNode* node=q.front();
+//                 q.pop();
+//                 if(node->left!=NULL)q.push(node->left);
+//                 if(node->right!=NULL)q.push(node->right);
+                
+//             }
+//             ans++;
+//         }
+//         return ans;
     
     
     
