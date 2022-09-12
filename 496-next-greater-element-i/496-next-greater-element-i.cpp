f9db4ca7635c3de471a3d1d7c66b297dbc54eeed
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-12 19:11:26 +0530

    Time: 94 ms (5.01%), Space: 8.4 MB (95.10%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -2,33 +2,53 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-        int n=nums1.size();
+//         int n=nums1.size();
         
-        vector<int>ans;
+//         vector<int>ans;
+//         for(int i=0;i<nums1.size();i++)
+//         {
+//             for(int j=0;j<nums2.size();j++)
+//             {
+//                 if(nums1[i]==nums2[j])
+//                 {
+//                     int maxv=-1;
+//                       for(int k=j;k<nums2.size();k++)
+//                       { 
+//                           if(nums2[k]>nums1[i])
+//                           {
+//                              maxv=nums2[k];
+//                               break;
+//                           }
+//                       }
+                 
+//                     ans.push_back(maxv);
+                   
+                        
+//                 }
+                    
+//             }
+//         }
+//         return ans;
+        
+    vector<int>ans;
         for(int i=0;i<nums1.size();i++)
         {
             for(int j=0;j<nums2.size();j++)
             {
                 if(nums1[i]==nums2[j])
                 {
-                    int maxv=-1;
-                      for(int k=j;k<nums2.size();k++)
-                      { 
-                          if(nums2[k]>nums1[i])
-                          {
-                             maxv=nums2[k];
-                              break;
-                          }
-                      }
-                 
-                    ans.push_back(maxv);
-                   
-                        
+                    int mx=-1;
+                    for(int k=j+1;k<nums2.size();k++)
+                    {
+                        if(nums2[k]>nums1[i])
+                        {   mx=nums2[k];
+                            break;
+                        }
+                    }
+                    ans.push_back(mx);
                 }
-                    
             }
         }
         return ans;
-        
     }
 };
