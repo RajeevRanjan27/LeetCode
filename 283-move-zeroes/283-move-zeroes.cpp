f879218ff9188aeb84328aaa7fb288c81389dff4
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-13 22:55:20 +0530

    Time: 49 ms (29.73%), Space: 19 MB (98.00%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -17,20 +17,39 @@ public:
         // }
         // nums=ans;
         
+//         int j=0;
+        
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]!=0)
+//             {
+//                 nums[j++]=nums[i];
+//             }
+//         }
+//         for(;j<nums.size();j++)
+//         {
+//             nums[j]=0;
+//         }
+        
+        
         int j=0;
-        
-        
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]!=0)
             {
                 nums[j++]=nums[i];
+                
             }
         }
         for(;j<nums.size();j++)
         {
             nums[j]=0;
+            
         }
+    
+      
         
+      
     }
 };
