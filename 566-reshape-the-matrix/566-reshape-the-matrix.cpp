9e8921bc9e91801725eaa5a08186cb50d67fb2fc
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-13 23:47:29 +0530

    Time: 21 ms (32.20%), Space: 11.1 MB (28.23%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -1,54 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-//         int n=mat.size();
-//         int m=mat[0].size();
-//         vector<vector<int>>ans(r,vector<int>(c));
-//         vector<int>temp;
-//         int count=0;
-//       if(r*c!=m*n)
-//           return mat;
-    
-//       for(int i=0;i<n;i++)
-//       {
-//           for(int j=0;j<m;j++)
-//           {
-            
-//               temp.push_back(mat[i][j]);
-//           }
-//       }
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>ans(r,vector<int>(c));
+        vector<int>temp;
+        int count=0;
+      if(r*c!=m*n)
+          return mat;
+        int cptr=0,rptr=0;
+       for(int i=0;i<n;i++)
+         {
+             for(int j=0;j<m;j++)
+             {
+                 ans[rptr][cptr++]=mat[i][j];
+                  if(cptr==c)
+                  {
+                      cptr=0;
+                      rptr++;
+                  }
+             }
+         }
+return ans;
         
-//         for(int i=0;i<r;i++)
+        
+//         int m=mat.size();
+//         int n=mat[0].size();
+        
+//         if(m*n!=r*c)
 //         {
-//             for(int j=0;j<c;j++)
+//             return mat;
+//         }
+//         vector<vector<int>>ans(r,vector<int>(c));
+//         int rptr=0,cptr=0;
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
 //             {
-//                 ans[i][j]=temp[count++];
+//                 ans[rptr][cptr++]=mat[i][j];
+//                 if(cptr==c)
+//                 {
+//                     rptr++;cptr=0;
+//                 }
 //             }
 //         }
 //         return ans;
-        
-        
-        int m=mat.size();
-        int n=mat[0].size();
-        
-        if(m*n!=r*c)
-        {
-            return mat;
-        }
-        vector<vector<int>>ans(r,vector<int>(c));
-        int rptr=0,cptr=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                ans[rptr][cptr++]=mat[i][j];
-                if(cptr==c)
-                {
-                    rptr++;cptr=0;
-                }
-            }
-        }
-        return ans;
         
         
     }
