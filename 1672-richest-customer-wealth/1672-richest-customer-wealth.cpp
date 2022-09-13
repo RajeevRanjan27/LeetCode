0d5d98df728deebb5213b456e095f1534af6b6f4
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-13 22:58:49 +0530

    Time: 9 ms (49.71%), Space: 7.8 MB (51.09%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -18,15 +18,15 @@ public:
             
 //         }
 //         return ans;
-            int ans=0;
+//       
+        
+        int ans=0;
         for(int i=0;i<accounts.size();i++)
         {
             int sum=accumulate(accounts[i].begin(),accounts[i].end(),0);
             ans=max(ans,sum);
         }
-        return ans;        
-        
-        
+        return ans;
         
     }
 };
