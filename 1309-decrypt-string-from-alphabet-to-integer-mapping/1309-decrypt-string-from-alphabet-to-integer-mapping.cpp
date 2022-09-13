617c771b932ae347fc361c980890c1442541f1d9
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-14 03:05:21 +0530

    Time: 4 ms (35.93%), Space: 6.2 MB (38.97%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -1,34 +1,63 @@
 class Solution {
 public:
     string freqAlphabets(string s) {
-        int n=s.length();
+//         int n=s.length();
+//         string ans="";
+//         cout<<ans<<"\n";
+//         for(int i=n-1;i>=0;)
+//         {
+//            if(s[i]=='#')
+//            {   
+// //               //(s[i-2]*10)+s[i-1];
+// //                 char temp=(((int)s[i-2]*10)+(int)s[i-1]+96); 
+               
+//                char temp=(s[i-2]-'0')*10+(s[i-1]-'0')+96;
+               
+               
+// //   
+//                ans+=temp;
+//                   i-=3;
+//            }
+//             else
+//             {
+//                 char tem=(s[i]-'0')+96;
+//               //  cout<<ans<<"\n";
+//                 ans+=tem;
+//                  i--;
+//             }
+            
+            
+//         }
+//      reverse(ans.begin(),ans.end());
+//         return ans;
+        
+        
         string ans="";
-        cout<<ans<<"\n";
-        for(int i=n-1;i>=0;)
-        {
-           if(s[i]=='#')
-           {   
-//               //(s[i-2]*10)+s[i-1];
-//                 char temp=(((int)s[i-2]*10)+(int)s[i-1]+96); 
+        int n=s.size()-1;
+       while(n>=0)
+       {
+           if(s[n]=='#')
+           {
+               if(s[n-2]=='1')
+               {
+                   char temp=106+(s[n-1]-'0');
+                   ans+=temp;
+                   
+               }
+               else{
+                   char temp=116+(s[n-1]-'0');
+                   ans+=temp;
+               }
+               n-=3;
                
-               char temp=(s[i-2]-'0')*10+(s[i-1]-'0')+96;
-               
-               
-//   
-               ans+=temp;
-                  i-=3;
            }
-            else
-            {
-                char tem=(s[i]-'0')+96;
-              //  cout<<ans<<"\n";
-                ans+=tem;
-                 i--;
-            }
-            
-            
-        }
-     reverse(ans.begin(),ans.end());
+           else {
+               char temp=96+(s[n]-'0');
+               ans+=temp;
+               n--;
+           }
+       }
+        reverse(ans.begin(),ans.end());
         return ans;
         
     }
