de38c6c01c585ce610d8f9ea084c9b29fe7390db
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-16 02:34:26 +0530

    Time: 26 ms (15.01%), Space: 11.6 MB (36.39%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -11,38 +11,59 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL)return  head;
-        // ListNode *p=head,*t=head->next;
-        // int pre=head->val;
-        // while(t!=NULL)
-        // {
-        //     if(pre!=t->val)
-        //     {
-        //         p->next=t;
-        //         pre=t->val;
-        //         t=t->next;
-        //     }
-        //     t=t->next;
-        // }
-        // p->next=NULL;
-        // return head;
+      if(head==NULL||head->next==NULL)return head;
         
-        ListNode *p=head,*q=head->next;
         
-        int prev=head->val;
-        while(q)
+        ListNode *p=head->next,*q=head;
+        int prev=q->val;
+        while(p)
         {
-            if(prev!=q->val)
+            if(p->val!=prev)
             {
-                p->next=q;
-                prev=q->val;
-                p=p->next;
+                q->next=p;
+               // p=p->next;
+                q=q->next;
+                prev=p->val;
+                
             }
-            q=q->next;
+            p=p->next;
+            
         }
-       p->next=NULL;
-        
+        q->next=NULL;
         return head;
+        
+//           if(head==NULL)return  head;
+//         // ListNode *p=head,*t=head->next;
+//         // int pre=head->val;
+//         // while(t!=NULL)
+//         // {
+//         //     if(pre!=t->val)
+//         //     {
+//         //         p->next=t;
+//         //         pre=t->val;
+//         //         t=t->next;
+//         //     }
+//         //     t=t->next;
+//         // }
+//         // p->next=NULL;
+//         // return head;
+        
+//         ListNode *p=head,*q=head->next;
+        
+//         int prev=head->val;
+//         while(q)
+//         {
+//             if(prev!=q->val)
+//             {
+//                 p->next=q;
+//                 prev=q->val;
+//                 p=p->next;
+//             }
+//             q=q->next;
+//         }
+//        p->next=NULL;
+        
+//         return head;
         
         
     }
