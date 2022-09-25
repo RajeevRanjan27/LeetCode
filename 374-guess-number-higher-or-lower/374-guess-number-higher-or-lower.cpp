62093c662f7742dc2601f7f300c26e315170fde7
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-09-25 12:51:57 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (68.16%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -9,30 +9,44 @@
 
 class Solution {
 public:
-    int guessNumber(int n) {
-       int high=n,low=1,x=0;
-        
-     while(high>=low)   {
-       x=low+((high-low)/2);
-        int res=guess(x);
-        
-        if(res==0)
-        {
-            cout<<x;
-            
-            break;
-        }
-        else if(res==-1)
-        {
-          //  cout<<low;
-            high=x-1;
-        }
-        else{
-            //cout<<high<<endl;
-            low=x+1;
-        }
-     }
-        return x;
-        
+    
+    long long numbercheck(long long low,long long high)
+    {
+        long long mid=low+((high-low)/2);
+        if(low>high) return -1;
+        if(guess(mid)==0)return mid;
+        else if (guess(mid)==-1) return numbercheck(low,mid-1);
+        else return numbercheck(mid+1,high);
     }
+    long long guessNumber(int n) {
+//        int high=n,low=1,x=0;
+        
+//      while(high>=low)   {
+//        x=low+((high-low)/2);
+//         int res=guess(x);
+        
+//         if(res==0)
+//         {
+//             cout<<x;
+            
+//             break;
+//         }
+//         else if(res==-1)
+//         {
+//           //  cout<<low;
+//             high=x-1;
+//         }
+//         else{
+//             //cout<<high<<endl;
+//             low=x+1;
+//         }
+//      }
+//         return x;
+        
+//     }
+        
+       
+        return numbercheck(0,n);
+    }
+        
 };
