ed290afbd630c43e9e2aae2de9abd9572a524dbb
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-10-10 23:32:46 +0530

    Time: 5 ms (31.61%), Space: 7.1 MB (33.39%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+//        for(int i=0;i<matrix.size();i++)
+//        {
+//           for(int j=i+1;j<matrix.size();j++) 
+//           {
+//             swap(matrix[i][j],matrix[j][i]);
+//           }
+        
+//        }
+//         for(int i=0;i<matrix.size();i++)
+//         {
+//             for(int j=0;j<matrix.size()/2;j++)
+//             {
+//                 swap(matrix[i][j],matrix[i][matrix.size()-j-1]);
+//             }
+//         }
+        
+        
+        
+        //rotate array
+        
+//         for(int i=0;i<matrix.size();i++)
+//         {
+//             for(int j=i+1;j<matrix.size();j++)
+//             {
+//                 swap(matrix[i][j],matrix[j][i]);
+//             }
+//         }
+//         for(int i=0;i<matrix.size();i++)
+//         {
+//             reverse(matrix[i].begin(),matrix[i].end());
+//         }
+        
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=i+1;j<matrix.size();j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<matrix.size();i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+            
+        }
+        
+    }
+};
