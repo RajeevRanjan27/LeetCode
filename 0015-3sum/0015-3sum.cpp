e64dfea88ec4646137c3e7423783fd99706b503b
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-10-25 21:34:20 +0530

    Time: 95 ms (90.66%), Space: 19.9 MB (85.37%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,21 +3,63 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
     
-       sort(nums.begin(),nums.end());
+//        sort(nums.begin(),nums.end());
         
-       vector<vector<int>>res;
+//        vector<vector<int>>res;
+//         int n=nums.size();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(i>0 && nums[i]==nums[i-1])continue;
+            
+//             int left=i+1;
+//             int right=n-1;
+            
+//             while(left<right)
+//             {
+//                 int sum=nums[i]+nums[left]+nums[right];
+//                 if(sum>0)
+//                 {
+//                     right--;
+//                 }
+//                 else if(sum<0)
+//                 {
+//                     left++;
+//                 }
+//                 else {
+//                     res.push_back(vector<int> {nums[i],nums[left],nums[right]});
+               
+//                           while(right>0 && nums[right]==nums[right-1])
+//                             {
+//                                right--;
+//                             }
+//                           while(left<n-1 && nums[left]==nums[left+1])
+//                            {
+//                              left++;
+//                            }
+//                     right--,left++;
+            
+//                     }
+//             }
+//         }
+//         return res;
+        
+        
+        sort(nums.begin(),nums.end());
         int n=nums.size();
+        vector<vector<int>>ans;
         
         for(int i=0;i<n;i++)
         {
-            if(i>0 && nums[i]==nums[i-1])continue;
-            
+            if(i>0 && nums[i]==nums[i-1])
+            {
+                continue;
+            }
             int left=i+1;
             int right=n-1;
-            
             while(left<right)
             {
-                int sum=nums[i]+nums[left]+nums[right];
+               int sum=nums[left]+nums[right]+nums[i];
                 if(sum>0)
                 {
                     right--;
@@ -26,21 +68,26 @@ public:
                 {
                     left++;
                 }
-                else {
-                    res.push_back(vector<int> {nums[i],nums[left],nums[right]});
-                while(right>0 && nums[right]==nums[right-1])
-                {
+                else{
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                     
+                    while(left<n-1 && nums[left]==nums[left+1])
+                    {
+                        left++;
+                    }
+                    while(right>0 && nums[right]==nums[right-1])
+                    {
+                        right--;
+                    }
                     right--;
+                   left++;
+                    
+                    
                 }
-                while(left<n-1 && nums[left]==nums[left+1])
-                {
-                    left++;
-                }
-                    right--,left++;
-             }
+                
             }
-        }
-        return res;
         
+        }
+        return ans;
     }
 };
