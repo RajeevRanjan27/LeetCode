6840a429885fe7007c8232ed197532aa02bd1088
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2022-12-09 16:21:17 +0530

    Time: 12 ms (55.50%), Space: 8.4 MB (39.97%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,71 +10,74 @@
  */
 class Solution {
 public:
-     ListNode* getNodeat(int idx,ListNode* head)
-        {
-            ListNode* t=head;
-            for(int i=0;i<idx && t;i++)
-            {
-                t=t->next;
+//      ListNode* getNodeat(int idx,ListNode* head)
+//         {
+//             ListNode* t=head;
+//             for(int i=0;i<idx && t;i++)
+//             {
+//                 t=t->next;
                 
-            }
-            return t;
+//             }
+//             return t;
             
-        }
+//         }
     ListNode* reverseList(ListNode* head) {
-//        ListNode *curr=NULL;
-//         while(head)
+   
+//         ListNode* l1=head;
+        
+        
+//         int ri=0;
+//         int li=0;
+//         int s=0;
+        
+        
+//         while(l1)
 //         {
-//             ListNode *next=head->next;
-//             head->next=curr;
-//             curr=head;
-//             head=next;
+//             s++;
+//             l1=l1->next;
             
 //         }
-//         return curr;
-        
-//         ListNode *curr=NULL;
-//         while(head)
-//         {
-//             ListNode *next=head->next;
-            
-//         }
-       
-        ListNode* l1=head;
-        
-        
-        int ri=0;
-        int li=0;
-        int s=0;
-        
-        
-        while(l1)
-        {
-            s++;
-            l1=l1->next;
-            
-        }
-        ri=s-1;
+//         ri=s-1;
         
            
         
-        while(li<ri)
-        {
+//         while(li<ri)
+//         {
     
-       ListNode* left=getNodeat(li,head);
-       ListNode* right=getNodeat(ri,head);
+//        ListNode* left=getNodeat(li,head);
+//        ListNode* right=getNodeat(ri,head);
             
-        int temp=left->val;
-            left->val=right->val;
-            right->val=temp;
+//         int temp=left->val;
+//             left->val=right->val;
+//             right->val=temp;
             
-            li++;
-            ri--;
+//             li++;
+//             ri--;
+            
+//         }
+        
+//         return head;
+        
+     
+        
+        ListNode* curr=head;
+        ListNode* prev=nullptr ;
+        
+         
+        while(curr)
+        {
+            
+            ListNode *temp=curr->next;
+            
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+            // cout<<curr<<"  "<<prev<<"  ";
             
         }
+         // curr->next=prev;
         
-        return head;
-        
+        return prev;        
         
         
     }
