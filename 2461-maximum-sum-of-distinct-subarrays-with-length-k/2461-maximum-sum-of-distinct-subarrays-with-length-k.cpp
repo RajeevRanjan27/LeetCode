5dabbbbec4833e3d4bc53d469388b40bb5fa048e
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2023-01-20 02:52:14 +0530

    Time: 288 ms (69.90%), Space: 90.1 MB (52.21%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -9,19 +9,20 @@ public:
         
         int i=0,j=0;
         unordered_map<int,int>mp;
-        vector<long long>prefix(nums.size());
-        prefix[0]=nums[0];
+//         vector<long long>prefix(nums.size());
+//         prefix[0]=nums[0];
         
-        for(int i=1;i<nums.size();i++)
-        {
-            prefix[i]=prefix[i-1]+nums[i];
-        }
+//         for(int i=1;i<nums.size();i++)
+//         {
+//             prefix[i]=prefix[i-1]+nums[i];
+//         }
         
         
         
         while(j<nums.size())
         {
             mp[nums[j]]++;
+            temp+=nums[j];
             
             if(j-i+1<k)
             {
@@ -30,8 +31,9 @@ public:
             }
             else if(j-i+1==k&&mp.size()==k)
             {
-                    ans=max(ans,prefix[j]-prefix[i]+nums[i]);
+                    ans=max(ans,temp);
                     
+                    temp-=nums[i];
                     mp[nums[i]]--;
                     if(mp[nums[i]]==0)mp.erase(nums[i]);
                     i++;
@@ -43,7 +45,7 @@ public:
               while(mp.size()<j-i+1)
               {
                   mp[nums[i]]--;
-                  
+                  temp-=nums[i];
                   if(mp[nums[i]]==0)mp.erase(nums[i]);
                   i++;
               }
