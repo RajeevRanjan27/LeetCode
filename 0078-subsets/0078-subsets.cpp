f59af07cc87281fbaac72c45f88086bdba7ab214
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2023-02-10 14:38:29 +0530

    Time: 7 ms (24.70%), Space: 16.2 MB (22.77%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     
-    
-    vector<vector<int>>res;
-    
-    
-    void rajeev(vector<int>nums,vector<int>op)
+    void rajeev(vector<int>nums,vector<int>&op,vector<vector<int>>&res)
     {
         
         if(nums.size()==0)
@@ -15,21 +11,20 @@ public:
             
         }
         
-        vector<int>op1;
-        op1=op;
-        vector<int>op2;
-        op2=op;
+        vector<int>op1=op;
+        vector<int>op2=op;
         op2.push_back(nums[0]);
         nums.erase(nums.begin()+0);
-        rajeev(nums,op1);
-        rajeev(nums,op2);
+        rajeev(nums,op1,res);
+        rajeev(nums,op2,res);
         return;
     }
     vector<vector<int>> subsets(vector<int>& nums) 
         
     {
+        vector<vector<int>>res;
         vector<int>op;
-        rajeev(nums,op);
+        rajeev(nums,op,res);
         return res;
         
     }
