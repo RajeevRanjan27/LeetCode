43942a9435def8b04069604b5d1b5e27bc2f3ad2
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2023-04-05 17:25:34 +0530

    Time: 75 ms (5.07%), Space: 85.9 MB (5.01%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,17 +1,16 @@
 class Solution {
     
 private:
-    void dfs(int node,vector<int>adj[], vector<int>&vis)
+    void dfs(int node,vector<vector<int>>isConnected, vector<int>&vis)
     {
         vis[node]=1;
         
-        
-        for(auto it: adj[node])
+        for(int i=0;i<isConnected.size();i++)
         {
-            if(vis[it]==0)
+            if(vis[i]==0 && isConnected[node][i]==1)
             {
               
-                dfs(it,adj,vis);
+                dfs(i,isConnected,vis);
                 
             }
         }
@@ -23,28 +22,32 @@ public:
         
         int n=isConnected.size();
         // vector<int>adj[n+1];
-        vector<int>adj[n];
+//         vector<int>adj[n];
         
         
-        for(int i=0;i<n;i++)
-        {
+//         for(int i=0;i<n;i++)
+//         {
             
-            for(int j=0;j<n;j++)
-            {
-                if(i!=j && isConnected[i][j]==1)
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(i!=j && isConnected[i][j]==1)
                     
-                {
-                    adj[i].push_back(j);
-                     adj[j].push_back(i);
-                }
-            }
+//                 {
+//                     adj[i].push_back(j);
+//                      adj[j].push_back(i);
+//                 }
+//             }
              
-        }
+//         }
+        
+       
+        
+        if (n==0) return 0;
         
         
-        int start=0;
-        int cnt=0;
+         int cnt=0;
         vector<int>vis(n+1,0);
+        
         
         
         for(int i=0;i<n;i++)
@@ -52,7 +55,7 @@ public:
             if(vis[i]==0)
             {
                 cnt++;
-                dfs(i,adj,vis);
+                dfs(i,isConnected,vis);
                 
             }
         }
