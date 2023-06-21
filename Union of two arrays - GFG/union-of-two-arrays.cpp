65bd4b0bd56ae132efa8a3db7ce605969344292e
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2023-06-21 14:15:38 +0530

    Added solution - LeetHub

--- Union of two arrays - GFG/union-of-two-arrays.cpp
+++ Union of two arrays - GFG/union-of-two-arrays.cpp
@@ -12,60 +12,75 @@ class Solution{
     //Function to return the count of number of elements in union of two arrays.
     int doUnion(int a[], int n, int b[], int m)  {
         //code here
-        sort(a, a+n);
-        sort(b, b+m);
-        int last = -1;
+        // sort(a, a+n);
+        // sort(b, b+m);
+        // int last = -1;
         
-        int i=0,j=0;
-        int count=0;
+        // int i=0,j=0;
+        // int count=0;
         
-        while(i<n && j<m){
+        // while(i<n && j<m){
             
-            int temp=a[i];
+        //     int temp=a[i];
             
-            if(a[i]<b[j])
-            {
-                count++;
-                while(i<n && a[i]==temp )i++;
-            }
-            else if(a[i]>b[j])
-            {
-                temp = b[j];
-                count++;
-                while(j<m && b[j]==temp)j++;
-            }
-            else{
+        //     if(a[i]<b[j])
+        //     {
+        //         count++;
+        //         while(i<n && a[i]==temp )i++;
+        //     }
+        //     else if(a[i]>b[j])
+        //     {
+        //         temp = b[j];
+        //         count++;
+        //         while(j<m && b[j]==temp)j++;
+        //     }
+        //     else{
                 
-                while(i<n && a[i]==temp )i++;
-                while(j<m && b[j]==temp)j++;
-                count++;
-            }
-        }
+        //         while(i<n && a[i]==temp )i++;
+        //         while(j<m && b[j]==temp)j++;
+        //         count++;
+        //     }
+        // }
         
         
-        while(i<n){
-            if(last==a[i])
-            {
-                i++;
-                continue;
-            }
+        // while(i<n){
+        //     if(last==a[i])
+        //     {
+        //         i++;
+        //         continue;
+        //     }
                 
-            last=a[i];
-            count++;
-            i++;
-        }
-        while(j<m){
-           if(last==b[j])
-            {
-                j++;
-                continue;
-            }
+        //     last=a[i];
+        //     count++;
+        //     i++;
+        // }
+        // while(j<m){
+        //   if(last==b[j])
+        //     {
+        //         j++;
+        //         continue;
+        //     }
                 
-            last=b[j];
-            count++;
-            j++;
+        //     last=b[j];
+        //     count++;
+        //     j++;
+        // }
+        // return count;
+        int count =0;
+        
+        unordered_map<int,int>mp;
+        
+        for(int i=0;i<max(n,m);i++){
+            if(i<n){mp[a[i]]++;}
+            if(i<m){mp[b[i]]++;}
         }
-        return count;
+        
+        //  for(auto x:mp){
+        //      if(x.second>=1)count++;
+             
+        //  }
+         
+        return mp.size();
         
     }
 };
