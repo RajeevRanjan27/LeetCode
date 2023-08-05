6137b89a76ef9172806e9f872a7a9c8cb98f13ad
Rajeev Ranjan <90333766+RajeevRanjan27@users.noreply.github.com>
2023-08-06 04:04:09 +0530

    Added solution - LeetHub

--- DFS of Graph - GFG/dfs-of-graph.cpp
+++ DFS of Graph - GFG/dfs-of-graph.cpp
@@ -4,35 +4,34 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    
-  private:
-   void dfs(int node, vector<int>adj[], int vis[],vector<int>&ls)
-   {
-       vis[node]=1;
-       ls.push_back(node);
-       
-       for(auto it: adj[node])
-       {
-           if(!vis[it])
-           {
-               dfs(it,adj,vis,ls);
-           }
-       }
-   }
   public:
     // Function to return a list containing the DFS traversal of the graph.
+    
+    
+    void dfs(int node,vector<int>adj[], vector<int> & vis, vector<int> & ans){
+        vis[node]=1;
+        ans.push_back(node);
+        
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                dfs(it,adj, vis, ans);
+            }
+        }
+    }
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
         // Code here
         
-        int vis[V]={0};
-        vector<int>ls;
-        int start=0;
-         dfs(start,adj,vis,ls);
-         return ls;
+        vector<int>ans;
+        vector<int>vis(V,0);
         
-        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,adj, vis, ans);
+            }
+        }
+        return ans;
     }
-};  
+};
 
 //{ Driver Code Starts.
 int main() {
